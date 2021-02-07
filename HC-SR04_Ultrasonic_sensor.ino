f215c5455c27c0facb2e7d66651fb8f96cf3d66d
Kontelis Spyros <spyroskontelis@gmail.com>
2021-02-07 19:54:01 +0200

    Created Arduino repo

---
+++ HC-SR04_Ultrasonic_sensor.ino
@@ -0,0 +1,46 @@
+
+// Hook up HC-SR04 with Trig to Arduino Pin 10, Echo to Arduino pin 13
+//Vcc has to be 5V
+#define trigPin 10
+#define echoPin 13
+
+float duration, distance;
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+   
+  // Write a pulse to the HC-SR04 Trigger Pin
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Measure the response from the HC-SR04 Echo Pin
+ 
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Determine distance from duration
+  // Use 343 metres per second as speed of sound
+  
+  distance = (duration / 2) * 0.0347;
+  
+  // Send results to Serial Monitor
+
+  Serial.print("Distance = ");
+  if (distance >= 400 || distance <= 2) {
+     Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(500);
+  }
+  delay(500);
+}
